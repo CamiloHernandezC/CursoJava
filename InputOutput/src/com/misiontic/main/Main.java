68b28eeba36a030e51979af1b8ef62f598adfe71
Camilo Hernandez <camehernandecas@unal.edu.co>
2021-07-23 20:49:20 -0500

    explicación entrada y salida de datos

--- InputOutput/src/com/misiontic/main/Main.java
+++ InputOutput/src/com/misiontic/main/Main.java
@@ -9,13 +9,13 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		//Salida de datos
-		/*System.out.println("Hello world"); //Tiene salto de linea
+		/*
+		System.out.println("Hello world"); //Tiene salto de linea
 		System.out.print("Hola mundo"); //No tiene salto de linea
 		System.out.print("Hola 2");
-		*/
+		
 		
 		//Entrada de datos
-		/*
 		Scanner scanner = new Scanner(System.in); //Se crea el lector
 
         System.out.println("Por favor ingrese su nombre");//Se pide un dato al usuario
@@ -29,8 +29,8 @@ public class Main {
         //Nótese que ya no hubo necesidad de usar parseInt() pues nextInt nos retorna un entero derectamente
 
         System.out.println("Gracias " + nombre + " en 10 años usted tendrá " + (edad + 10) + " años."); //Operacion numerica con la edad
-        */
-		
+       
+		*/
 		//Segunda opcion lectura de datos
 		//Notar que readLine() nos obliga a declarar IOException
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //Ya tenemos el "lector"
@@ -40,7 +40,7 @@ public class Main {
         String nombre = br.readLine(); //Se lee el nombre con readLine() que retorna un String con el dato
 
         System.out.println("Bienvenido " + nombre + ". Por favor ingrese su edad");//Se pide otro dato al usuario
-
+        
         String entrada = br.readLine(); //Se guarda la entrada (edad) en una variable
 
         //Nótese que readLine siempre retorna String y la clase BufferedReader...
