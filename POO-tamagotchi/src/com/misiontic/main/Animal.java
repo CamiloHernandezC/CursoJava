35452977789befb8d6b2b2f4189e35d949ff2725
Camilo Hernandez <camehernandecas@unal.edu.co>
2021-07-30 07:38:26 -0500

    improve of eat function

--- POO-tamagotchi/src/com/misiontic/main/Animal.java
+++ POO-tamagotchi/src/com/misiontic/main/Animal.java
@@ -3,35 +3,28 @@ package com.misiontic.main;
 import java.awt.Color;
 
 public class Animal {
-	int cantidadDePatas = 4;
-	Color colordePelo;
+	
 	float peso;
-	static int felicidad;//va de 0 a 100, 0 es triste; 100 es feliz.
+	int felicidad;//va de 0 a 100, 0 es triste; 100 es feliz.
 	int salud;//va de 0 a 100, 0 muerto, 100 muy saludable.
-	int hambre;//va de 0 a 100, 0 es famelico y 100 es muy lleno.
+	int hambre;//va de 0 a 100, 0 es sin hambre y 100 es famelico.
+	int sueno; //va de 0 a 100, 0 es muy despierto y 100 es muy somnoliento
 	
 	//Tengo que dar una respuesta cuando termine de comer?
 	//Yo quiero decir si ya termine de comer
 	private void comer() {
-		if(hambre >= 100) {
-			hambre = 100;
+		if(hambre <= 5) {
+			hambre = 0;
 			System.out.println("Estoy demasiado lleno, me voy a enfermar de tanto comer");
 			salud = salud -2;
 		}else {
-			if(hambre + 5 > 100) {
-				hambre = 100;
-			}else {
-				hambre = hambre +5;
-			}
+			hambre = hambre - 5;
 		}
-		
 	}
 	private void dormir() {
 		
 	}
-	private void irAlDoctor() {
-		
-	}
+
 	private void ejercitar() {
 		
 	}
--- POO-tamagotchi/src/com/misiontic/main/Main.java
+++ POO-tamagotchi/src/com/misiontic/main/Main.java
@@ -3,19 +3,17 @@ package com.misiontic.main;
 public class Main {
 
 	public static void main(String[] args) {
-		Animal perrito = new Animal();
-		perrito.hambre = 50;
-		perrito.felicidad = 50;
 		
-		Animal gatico = new Animal();
-		gatico.hambre = 70;
-		gatico.felicidad = 70;
+		Animal gato = new Animal();		
+		gato.peso = 10;
+		gato.felicidad = 100;//va de 0 a 100, 0 es triste; 100 es feliz.
+		gato.salud = 100;//va de 0 a 100, 0 muerto, 100 muy saludable.
+		gato.hambre = 0;//va de 0 a 100, 0 es sin hambre y 100 es famelico.
+		gato.sueno = 0; //va de 0 a 100, 0 es muy despierto y 100 es muy somnoliento
+
 		
-		System.out.println("hambre perrito: " + perrito.hambre);
-		System.out.println("hambre gatico: " + gatico.hambre);
+		System.out.println("hambre gatico: " + gato.hambre);
 		
-		System.out.println("felicidad perrito: " + perrito.felicidad);
-		System.out.println("felicidad gatico: " + gatico.felicidad);
 
 	}
 
